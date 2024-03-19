6b90723e07234317de8a0368437f1c12c9f3f495
Name <shi-666@yandex.ru>
2024-03-19 23:15:39 +0300

    ez app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+// массив вопросов и ответов. Можно больше вариантов, вожно меньше. Пока без кнопки подтверждения ответа
+const questions = [
+  { question: 'Вопрос 1', options: ['Ответ 1', 'Ответ 2', 'Ответ 3'], correctAnswer: 1 },
+  { question: 'Вопрос 2', options: ['Ответ 1', 'Ответ 2', 'Ответ 3'], correctAnswer: 2 }
+  // здесь другие вопросы и варианты
+];
+
+const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]); // хукнем useState для управления состоянием текущего вопроса и ответов. useState для хранения и обновления информации о текущем вопросе и ответах, не забудь
+
+// тут обработаем выбор ответа на вопрос
+  const handleAnswer = (answer) => {
+    setAnswers([...answers, answer]);  // добавляем ответ в массив
+    setCurrentQuestion(currentQuestion + 1); // и переходим к следующему вопросу, если он есть. Ниже обработаем
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentQuestion < questions.length ? (     // проверка, есть ли еще вопросы
+        <div>
+          <h3>{questions[currentQuestion].question}</h3>   
+          {questions[currentQuestion].options.map((option, index) => {
+            return (
+              <button key={index} onClick={() => handleAnswer(index)}>{option}</button> // cоздание кнопок для вариантов ответов. Кнопка для подтверждения ответа будет после этого по идее
+            );
+          })}
+        </div>
+      ) : (
+        <div>
+          <h3>Результаты опроса:</h3>
+          <ul>
+            {answers.map((answer, index) => {
+              return (
+                <li key={index}>{questions[index].question}: {questions[index].options[answer]}</li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
